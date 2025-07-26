daa9565b594673a3861761ec3626611f3ecd7365
Nick Shao <nckshao@gmail.com>
2025-07-25 19:26:58 -0600

    .

--- DSA/LinkedLists/LinkedList.java
+++ DSA/LinkedLists/LinkedList.java
@@ -1,7 +1,7 @@
 package DSA.LinkedLists;
 
-public class LinkedList {
 
+public class LinkedList {
     private Node head;
     private Node tail;
     private int length;
@@ -15,20 +15,25 @@ public class LinkedList {
         }
     }
 
+
     public LinkedList(int value) {
         Node newNode = new Node(value);
+
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
+
     public void printList() {
         Node temp = head;
+
         while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
+
 
     public void getHead() {
         if (head == null) {
@@ -38,6 +43,7 @@ public class LinkedList {
         }
     }
 
+
     public void getTail() {
         if (head == null) {
             System.out.println("Tail: null");
@@ -46,12 +52,15 @@ public class LinkedList {
         }
     }
 
+
     public void getLength() {
         System.out.println("Length: " + length);
     }
 
+
     public void append(int value) {
         Node newNode = new Node(value);
+
         if (length == 0) {
             head = newNode;
             tail = newNode;
@@ -62,17 +71,21 @@ public class LinkedList {
         length++;
     }
 
+
     public Node removeLast() {
         if (length == 0) return null;
+
         Node temp = head;
         Node pre = head;
         while(temp.next != null) {
             pre = temp;
             temp = temp.next;
         }
+
         tail = pre;
         tail.next = null;
         length--;
+
         if (length == 0) {
             head = null;
             tail = null;
@@ -80,8 +93,10 @@ public class LinkedList {
         return temp;
     }
 
+
     public void prepend(int value) {
         Node newNode = new Node(value);
+
         if (length == 0) {
             head = newNode;
             tail = newNode;
@@ -92,26 +107,32 @@ public class LinkedList {
         length++;
     }
 
+
     public Node removeFirst() {
         if (length == 0) return null;
+
         Node temp = head;
         head = head.next;
         temp.next = null;
         length--;
+
         if (length == 0) {
             tail = null;
         }
         return temp;
     }
 
+
     public Node get(int index) {
         if (index < 0 || index >= length) return null;
+
         Node temp = head;
         for(int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp;
     }
+
 
     public boolean set(int index, int value) {
         Node temp = get(index);
@@ -122,8 +143,10 @@ public class LinkedList {
         return false;
     }
 
+
     public boolean insert(int index, int value)  {
         if (index < 0 || index > length) return false;
+
         if (index == 0) {
             prepend(value);
             return true;
@@ -132,13 +155,16 @@ public class LinkedList {
             append(value);
             return true;
         }
+
         Node newNode = new Node(value);
         Node temp = get(index - 1);
         newNode.next = temp.next;
         temp.next = newNode;
         length++;
+
         return true;
     }
+
 
     public Node remove(int index) {
         if (index < 0 || index >= length) return null;
@@ -151,8 +177,10 @@ public class LinkedList {
         prev.next = temp.next;
         temp.next = null;
         length--;
+
         return temp;
     }
+
 
     public void reverse() {
         Node temp = head;
@@ -160,6 +188,7 @@ public class LinkedList {
         tail = temp;
         Node after = temp.next;
         Node before = null;
+
         for (int i = 0; i < length; i++) {
             after = temp.next;
             temp.next = before;
@@ -167,5 +196,6 @@ public class LinkedList {
             temp = after;
         }
     }
-
 }
+
+
