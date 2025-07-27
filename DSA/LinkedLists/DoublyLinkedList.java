0e62d29e50352ff9758f4aea4bfbc566bf848fdf
Nick Shao <nckshao@gmail.com>
2025-07-27 12:21:17 -0600

    .

--- DSA/LinkedLists/DoublyLinkedList.java
+++ DSA/LinkedLists/DoublyLinkedList.java
@@ -22,20 +22,25 @@ public class DoublyLinkedList {
         }
     }
 
+
     public DoublyLinkedList(int value) {
         Node newNode = new Node(value);
+
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
+
     public void printList() {
         Node temp = head;
+
         while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
+
 
     public void getHead() {
         if (head == null) {
@@ -45,6 +50,7 @@ public class DoublyLinkedList {
         }
     }
 
+
     public void getTail() {
         if (head == null) {
             System.out.println("Tail: null");
@@ -53,12 +59,15 @@ public class DoublyLinkedList {
         }
     }
 
+
     public void getLength() {
         System.out.println("Length: " + length);
     }
 
+
     public void append (int value) {
         Node newNode = new Node(value);
+
         if(length == 0) {
             head = newNode;
             tail = newNode;
@@ -70,8 +79,10 @@ public class DoublyLinkedList {
         length++;
     }
 
+
     public Node removeLast() {
         if(length == 0) return null;
+
         Node temp = tail;
         if (length == 1) {
             head = null;
@@ -82,11 +93,14 @@ public class DoublyLinkedList {
             temp.prev = null;
         }
         length--;
+
         return temp;
     }
 
+
     public void prepend(int value) {
         Node newNode = new Node(value);
+
         if(length == 0) {
             head = newNode;
             tail = newNode;
@@ -98,8 +112,10 @@ public class DoublyLinkedList {
         length++;
     }
 
+
     public Node removeFirst() {
         if(length == 0) return null;
+
         Node temp = head;
         if(length == 1) {
             head = null;
@@ -110,17 +126,20 @@ public class DoublyLinkedList {
             temp.next = null;
         }
         length--;
+
         return temp;
     }
 
-    public Node get(int index) {
+
+    public Node get(int index) {                            // NOT O(log n), still traverse linearly
         if (index < 0 || index >= length) return null;
+
         Node temp = head;
-        if (index < length/2) {
+        if (index < length / 2) {                           // Traverse from the head
             for (int i = 0; i < index; i++) {
                 temp = temp.next;
             }
-        } else {
+        } else {                                            // Traverse from the tail
             temp = tail;
             for (int i = length - 1; i > index; i--) {
                 temp = temp.prev;
@@ -129,8 +148,10 @@ public class DoublyLinkedList {
         return temp;
     }
 
+
     public boolean set(int index, int value) {
         Node temp = get(index);
+
         if(temp != null) {
             temp.value = value;
             return true;
@@ -138,8 +159,10 @@ public class DoublyLinkedList {
         return false;
     }
 
+
     public boolean insert(int index, int value) {
         if(index < 0 || index > length) return false;
+
         if(index == 0) {
             prepend(value);
             return true;
@@ -148,6 +171,7 @@ public class DoublyLinkedList {
             append(value);
             return true;
         }
+
         Node newNode = new Node(value);
         Node before = get(index - 1);
         Node after = before.next;
@@ -156,8 +180,10 @@ public class DoublyLinkedList {
         before.next = newNode;
         after.prev = newNode;
         length++;
+
         return true;
     }
+
 
     public Node remove(int index) {
         if(index < 0 || index >= length) return null;
@@ -170,8 +196,9 @@ public class DoublyLinkedList {
         temp.prev.next = temp.next;
         temp.next = null;
         temp.prev = null;
-
         length--;
+        
         return temp;
     }
 }
+
