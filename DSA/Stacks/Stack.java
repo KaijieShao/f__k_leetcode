0c4190f35314580e6b90615aaeb6e15da20c0de5
Nick Shao <nckshao@gmail.com>
2025-07-27 13:46:46 -0600

    .

--- DSA/Stacks/Stack.java
+++ DSA/Stacks/Stack.java
@@ -1,9 +1,15 @@
 package DSA.Stacks;
 
+
 public class Stack {
 
-    private Node top;
-    private int height;
+    // Scenario:
+    // Solve problems where you need to manage data in a Last-In, First-Out (LIFO) order i.e., backtracking, undo operations etc
+    // Choose ArrayList for better cache performance and less memory per item
+    // choose LinkedList for frequent dynamic resizing and simple O(1) operations without resizing costs.
+
+    private Node top;                                 // All operations (push, pop, peek) happen at the top, so ONLY track the top
+    private int height;                               // Track how many nodes are in the stack
 
     class Node {
         int value;
@@ -14,19 +20,22 @@ public class Stack {
         }
     }
     
+
     public Stack(int value) {
         Node newNode = new Node(value);
         top = newNode;
         height = 1;
     }
 
-    public void printStack() {
+
+    public void printStack() {                        // Print the stack from top to bottom
         Node temp = top;
         while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
+
 
     public void getTop() {
         if (top == null) {
@@ -36,30 +45,34 @@ public class Stack {
         }
     }
 
+
     public void getHeight() {
         System.out.println("Height: " + height);
     }
+
 
     public void push(int value) {
         Node newNode = new Node(value);
         if(height == 0) {
             top = newNode;
         } else {
-            newNode.next = top;
-            top = newNode;
+            newNode.next = top;                     // Set newNode.next to the current top
+            top = newNode;                          // Update top to be the new node
         }
         height++;
     }
 
+
     public Node pop() {
-        if(height == 0) return null;
+        if (height == 0) return null;
 
         Node temp = top;
         top = top.next;
-        temp.next = null;
-
+        temp.next = null;                           // Disconnect the popped node: not strictly necessary, but a good practice
         height--;
+
         return temp;
     }
-
 }
+
+
