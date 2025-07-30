c1364c749b3f0ec327b20721af0b8b8298d91743
Nick Shao <nckshao@gmail.com>
2025-07-30 14:03:43 -0600

    .

--- DSA/DPs/Top_down.java
+++ DSA/DPs/Top_down.java
@@ -1,44 +1,38 @@
 package DSA.DPs;
 
+
 public class Top_down {
 
+    // Scenario:
+    // Problems that require dynamic programming (DP) usually have:
+    // 1. Overlapping subproblems: repeated subproblems get solved multiple times i.e., fib(2) recalculated 2 times
+    // 2. Optimal substructure: combine optimal parts to get the optimal whole
+
+    // Top-Down: Starts from the main problem (fib(n)) and recursively solves subproblems
+    // Memoization: Stores results of subproblems (memo[n]) to avoid redundant calculations
+    
     static Integer[] memo = new Integer[100];
     static int counter = 0;
 
     public static int fib(int n) {
         counter++;
 
-         if (memo[n] != null) { 
+         if (memo[n] != null) {              
                 return memo[n]; 
             }
-    
             if (n == 0 || n == 1) { 
                 return n; 
             }
-    
             memo[n] = fib(n - 1) + fib(n - 2); 
     
             return memo[n]; 
     }
 
     public static void main(String[] args) {
-
         int n = 20;
-
         System.out.println("\nFib of " + n + " = " + fib(n));
-
         System.out.println("\nCounter: " + counter);
-
     }
-
 }
-
-// Fib of 7 = 13
-
-// Counter: 13
-
-// Fib of 20 = 6765
-
-// Counter: 39
 
 
