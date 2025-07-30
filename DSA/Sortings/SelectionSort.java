60edd58874eb3e7e6360ad4bd17eb640ac485dea
Nick Shao <nckshao@gmail.com>
2025-07-30 10:01:51 -0600

    .

--- DSA/Sortings/SelectionSort.java
+++ DSA/Sortings/SelectionSort.java
@@ -1,19 +1,25 @@
 package DSA.Sortings;
 
+
 import java.util.Arrays;
 
 public class SelectionSort {
 
+    // Scenario:
+    // Repeatedly selects smallest (or largest) element from unsorted part and moves it to the correct position
+    // Selection Sort, Bubble Sort, and Insertion Sort all solve small datasets (e.g., n < 100)
+    // All have O(n²) time complexity, making them inefficient for big/real-world datasets
+
     public static void selectionSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            int minIndex = i;
-            for (int j = i+1; j < array.length; j++) {
+            int minIndex = i;                           
+            for (int j = i+1; j < array.length; j++) {   // Sortings are classic, intuitive example of nested loops
                 if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
             }
-            if (i != minIndex) {
-                int temp = array[i];
+            if (i != minIndex) {                         // Checker: avoid unnecessary swaps 
+                int temp = array[i];                     // Now, 'minIndex' hold smallest value -> swap it
                 array[i] = array[minIndex];
                 array[minIndex] = temp;
             }
@@ -21,18 +27,13 @@ public class SelectionSort {
     }
 
 
-
     public static void main(String[] args) {
-
         int[] myArray = {4,2,6,5,1,3};
-
         selectionSort(myArray);
-
         System.out.println( Arrays.toString(myArray) );
 
     }
-
 }
 
-// [1, 2, 3, 4, 5, 6]
+
 
