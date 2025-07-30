a3c979936111a643c7a1d58d24473f79a736b535
Nick Shao <nckshao@gmail.com>
2025-07-30 10:15:31 -0600

    .

--- DSA/Sortings/InsertionSort.java
+++ DSA/Sortings/InsertionSort.java
@@ -1,18 +1,21 @@
 package DSA.Sortings;
 
+
 import java.util.Arrays;
 
 public class InsertionSort {
 
-    public static void insertionSort(int[] array) {
+    // Scenario:
+    // Great analogy - InsertionSort is like building up a winning hand in Mahjong
+    // You pick up one item at a time, compare it to what's already sorted, and insert it in the correct position
 
+    // O(n^2) time, O(1) space
+    public static void insertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int temp = array[i];
             int j = i - 1;
-
-            while (j > -1 && temp < array[j]) {
-
-                array[j + 1] = array[j];
+            while (j > -1 && temp < array[j]) {        // If the array is sorted, the while loop is never triggered
+                array[j + 1] = array[j];               // Shifting the value right
                 array[j] = temp;
                 j--;
             }
@@ -20,18 +23,11 @@ public class InsertionSort {
     }
 
 
-
     public static void main(String[] args) {
-
         int[] myArray = {4,2,6,5,1,3};
-
         insertionSort(myArray);
-
         System.out.println( Arrays.toString(myArray) );
-
     }
-
 }
 
 
-// [1, 2, 3, 4, 5, 6]
